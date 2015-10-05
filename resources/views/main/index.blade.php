574a5fc2370a1cbde0eb08cc861bf6e8643d7594
iris mini <iris82720@gmail.com>
2015-10-05 16:23:31 +0800

    新增主頁(index.blade.php)

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,471 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="contentSection clearfix"><img src="images/dummy/not_login_top.jpg" /></div>
+    <div class="contentSection clearfix">
+        <div class="floatLeft col-md-7">
+            <div class="col-md-3 normal pink"><strong>200</strong> 位活躍會員 </div>
+            <div class="col-md-3 normal pink"><strong>150</strong> 件優質作品</div>
+            <div class="col-md-3 normal pink"><strong>60</strong> 件角色設定</div>
+            <div class="col-md-3 normal pink"><strong>30</strong> 個原創企劃</div>
+        </div>
+        <div class="right col-md-5 normal black">
+            註冊後就能自由投稿，快來與大家共同創作吧！
+        </div>
+    </div>
+    <div class="contentSection row">
+        <div class="annonucementBlock myInterfaceContentBlock col-md-12">
+            <div class="blockHeader">
+                網站公告
+
+                <a href="#" class="moreBtn">
+                    更多公告
+                    <span>+</span>
+                </a>
+            </div>
+            <div class="blockContent clearfix">
+                <div class="item clearfix">
+                    <div class="title">
+                        註冊後就能自由投稿，快來與大家共同創作吧！
+                    </div>
+                    <div class="date">
+                        2018/05/06
+                    </div>
+                </div>
+                <div class="item clearfix">
+                    <div class="title">
+                        註冊後就能自由投稿，快來與大家共同創作吧！
+                    </div>
+                    <div class="date">
+                        2018/05/06
+                    </div>
+                </div>
+                <div class="item clearfix">
+                    <div class="title">
+                        註冊後就能自由投稿，快來與大家共同創作吧！
+                    </div>
+                    <div class="date">
+                        2018/05/06
+                    </div>
+                </div>
+                <div class="item clearfix">
+                    <div class="title">
+                        註冊後就能自由投稿，快來與大家共同創作吧！
+                    </div>
+                    <div class="date">
+                        2018/05/06
+                    </div>
+                </div>
+
+            </div>
+            <span class="clearfix">	</span>
+
+        </div>
+    </div>
+    <div class="contentSection row">
+        <div class="myInterfaceContentBlock col-4 col-md-12">
+            <div class="blockHeader">
+                原創企劃
+                <div class="firstLvTab">
+                    <ul>
+                        <li><a href="">最新的企劃</a></li>
+                        <li><a href="">最熱門的企劃</a></li>
+                        <li><a href="">推薦的企劃</a></li>
+
+                    </ul>
+
+                </div>
+                <a href="#" class="moreBtn">
+                    更多公告
+                    <span>+</span>
+                </a>
+            </div>
+            <div class="blockContent clearfix projectList">
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">企劃名稱</span>
+                            <span class="date">2015/05/06</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">企劃名稱</span>
+                            <span class="date">2015/05/06</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">企劃名稱</span>
+                            <span class="date">2015/05/06</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="item last">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">企劃名稱</span>
+                            <span class="date">2015/05/06</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+            </div>
+            <span class="clearfix">	</span>
+        </div>
+    </div>
+    <div class="contentSection row">
+        <div class="myInterfaceContentBlock col-6 col-md-12">
+            <div class="blockHeader">
+                角色設定
+                <div class="firstLvTab">
+                    <ul>
+                        <li><a href="">最新的角色</a></li>
+                        <li><a href="">最熱門的角色</a></li>
+                        <li><a href="">推薦的角色</a></li>
+
+                    </ul>
+                </div>
+                <a href="#" class="moreBtn">
+                    更多角色
+                    <span>+</span>
+                </a>
+            </div>
+            <div class="blockContent clearfix charaList">
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">角色名稱</span>
+                        <span class="author">作者名稱</span>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                    <div class="image">
+                        <img src="images/dummy/artwork.jpg" />
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">角色名稱</span>
+                        <span class="author">作者名稱</span>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                    <div class="image">
+                        <img src="images/dummy/artwork.jpg" />
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">角色名稱</span>
+                        <span class="author">作者名稱</span>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                    <div class="image">
+                        <img src="images/dummy/artwork.jpg" />
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">角色名稱</span>
+                        <span class="author">作者名稱</span>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                    <div class="image">
+                        <img src="images/dummy/artwork.jpg" />
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">角色名稱</span>
+                        <span class="author">作者名稱</span>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                    <div class="image">
+                        <img src="images/dummy/artwork.jpg" />
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item last">
+                    <div class="desc">
+                        <span class="name">角色名稱</span>
+                        <span class="author">作者名稱</span>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                    <div class="image">
+                        <img src="images/dummy/artwork.jpg" />
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+
+            </div>
+            <span class="clearfix">	</span>
+
+        </div>
+    </div>
+    <div class="contentSection row">
+        <div class="myInterfaceContentBlock col-6 col-md-12">
+            <div class="blockHeader">
+                原創插畫
+                <div class="firstLvTab">
+                    <ul>
+                        <li><a href="">最新的插畫</a></li>
+                        <li><a href="">最熱門的插畫</a></li>
+                        <li><a href="">推薦的插畫</a></li>
+
+                    </ul>
+                </div>
+                <a href="#" class="moreBtn">
+                    更多插畫
+                    <span>+</span>
+                </a>
+            </div>
+            <div class="blockContent clearfix workList">
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">作品名稱</span>
+                            <span class="author">作者名</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">作品名稱</span>
+                            <span class="author">作者名</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">作品名稱</span>
+                            <span class="author">作者名</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">作品名稱</span>
+                            <span class="author">作者名</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">作品名稱</span>
+                            <span class="author">作者名</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="item last">
+                    <div class="image">
+                        <div class="desc">
+                            <span class="name">作品名稱</span>
+                            <span class="author">作者名</span>
+                        </div>
+                        <a href="#"><img src="images/dummy/project_thumb.jpg" /></a>
+                        <div class="count">
+                            <span class="comment">1</span>
+                            <span class="favourite">20</span>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+            <span class="clearfix">	</span>
+        </div>
+    </div>
+    <div class="contentSection row">
+        <div class="myInterfaceContentBlock col-6 col-md-12">
+            <div class="blockHeader">
+                原創小說
+                <div class="firstLvTab">
+                    <ul>
+                        <li><a href="">最新的小說</a></li>
+                        <li><a href="">最熱門的小說</a></li>
+                        <li><a href="">推薦的小說</a></li>
+
+                    </ul>
+                </div>
+                <a href="#" class="moreBtn">
+                    更多小說
+                    <span>+</span>
+                </a>
+            </div>
+            <div class="blockContent novelList clearfix">
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">作品名稱</span>
+                        <span class="author">投稿者</span>
+                    </div>
+                    <div class="preview">
+                        這裡為作者自行輸入的小說大鋼。限制在幾百字裏面完成?
+                    </div>
+                    <div class="count">
+                        <span class="comment">1</span>
+                        <span class="favourite">20</span>
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">作品名稱</span>
+                        <span class="author">投稿者</span>
+                    </div>
+                    <div class="preview">
+                        這裡為作者自行輸入的小說大鋼。限制在幾百字裏面完成?
+                    </div>
+                    <div class="count">
+                        <span class="comment">1</span>
+                        <span class="favourite">20</span>
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">作品名稱</span>
+                        <span class="author">投稿者</span>
+                    </div>
+                    <div class="preview">
+                        這裡為作者自行輸入的小說大鋼。限制在幾百字裏面完成?
+                    </div>
+                    <div class="count">
+                        <span class="comment">1</span>
+                        <span class="favourite">20</span>
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">作品名稱</span>
+                        <span class="author">投稿者</span>
+                    </div>
+                    <div class="preview">
+                        這裡為作者自行輸入的小說大鋼。限制在幾百字裏面完成?
+                    </div>
+                    <div class="count">
+                        <span class="comment">1</span>
+                        <span class="favourite">20</span>
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item">
+                    <div class="desc">
+                        <span class="name">作品名稱</span>
+                        <span class="author">投稿者</span>
+                    </div>
+                    <div class="preview">
+                        這裡為作者自行輸入的小說大鋼。限制在幾百字裏面完成?
+                    </div>
+                    <div class="count">
+                        <span class="comment">1</span>
+                        <span class="favourite">20</span>
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+                <div class="item last">
+                    <div class="desc">
+                        <span class="name">作品名稱</span>
+                        <span class="author">投稿者</span>
+                    </div>
+                    <div class="preview">
+                        這裡為作者自行輸入的小說大鋼。限制在幾百字裏面完成?
+                    </div>
+                    <div class="count">
+                        <span class="comment">1</span>
+                        <span class="favourite">20</span>
+                    </div>
+                    <a href="#" class="clickableMask">&nbsp;</a>
+                </div>
+
+
+
+
+            </div>
+            <span class="clearfix">	</span>
+        </div>
+    </div>
+@endsection
